73a20d20ca4ab65b49ae1a07b2577bcddfd97f4a
nbari <nbari@dalmp.com>
2016-03-18 14:50:56 +0100

    renamed:    handler.go -> tcp.go 	new file:   udp.go

---
+++ tcp.go
@@ -0,0 +1,23 @@
+package UDPProxy
+
+import (
+	"log"
+	"net"
+)
+
+func (self *UDPProxy) handlePacketTCP(i int, buf []byte) {
+	rConn, err := net.DialTCP("tcp", nil, self.tcp)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer rConn.Close()
+
+	if _, err := rConn.Write(buf[0:i]); err != nil {
+		log.Fatalln(err)
+	}
+
+	if self.debug {
+		log.Println(string(buf[0:i]))
+	}
+	return
+}
--- udp.go
+++ udp.go
@@ -5,23 +5,6 @@ import (
 	"net"
 )
 
-func (self *UDPProxy) handlePacketTCP(i int, buf []byte) {
-	rConn, err := net.DialTCP("tcp", nil, self.tcp)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	defer rConn.Close()
-
-	if _, err := rConn.Write(buf[0:i]); err != nil {
-		log.Fatalln(err)
-	}
-
-	if self.debug {
-		log.Println(string(buf[0:i]))
-	}
-	return
-}
-
 func (self *UDPProxy) handlePacketUDP(i int, buf []byte) {
 	rConn, err := net.DialUDP("udp", nil, self.udp)
 	if err != nil {
